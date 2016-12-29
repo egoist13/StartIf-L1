2410f4d7a87a630992faf2e31d47248f7897bd64
egoist13 <RuslanEGO@gmail.com>
2016-12-29 19:26:31 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 function showText(el) {
-    var currentHeight = el.parentElement.parentElement.parentElement.clientHeight;
+    var parentEl = el.parentElement.parentElement.parentElement;
+    var currentHeight = parentEl.clientHeight;
     if(el.previousElementSibling.className === "more") {
         el.previousElementSibling.className = "less";
-        el.parentElement.parentElement.parentElement.style.height = currentHeight+"px";
+        parentEl.style.height = currentHeight+"px";
         el.innerHTML = "Show less...";
     } else {
         el.previousElementSibling.className = "more";
-        el.parentElement.parentElement.parentElement.style.height = "auto";
+        parentEl.style.height = "auto";
         el.innerHTML = "Show more...";
     }
 }
