e5b40700756da311fafcbbfab2222aa1097f3bdc
egoist13 <RuslanEGO@gmail.com>
2016-12-29 19:28:45 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 function showText(el) {
-    var parentEl = el.parentElement.parentElement.parentElement;
-    var currentHeight = parentEl.clientHeight;
-    if(el.previousElementSibling.className === "more") {
+    var parentEl = el.parentElement.parentElement.parentElement, currentHeight = parentEl.clientHeight;
+    if (el.previousElementSibling.className === "more") {
         el.previousElementSibling.className = "less";
-        parentEl.style.height = currentHeight+"px";
+        parentEl.style.height = currentHeight + "px";
         el.innerHTML = "Show less...";
     } else {
         el.previousElementSibling.className = "more";
